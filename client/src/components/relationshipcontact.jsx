81da22dabb67fa8efb7e154b36feeef3bec89ef4
tushort <samquioh@gmail.com>
2023-09-27 17:52:20 -0700

    updates contactrelationship component to only include the array from the file

--- client/src/components/relationshipcontact.jsx
+++ client/src/components/relationshipcontact.jsx
@@ -1,36 +1,12 @@
 
-import { MenuItem, Select, FormControl, InputLabel } from '@mui/material'
+ 
 
-
-
-const RelationshipContact = () =>{
-
-const relationships = ['friend', 'colleague', 'family']; 
+export const relationships = ['friend', 'colleague', 'family', 'Business']; 
   
-
-  return (
-    
-      <div>
-      <FormControl variant="outlined" fullWidth>
-        <InputLabel htmlFor="relationship-select">Relationship</InputLabel>
-        <Select
-          label="Relationship"
-          labelId="relationship-select"
-          id="relationship-select"
-        >
-          {relationships.map((relationship) => (
-            <MenuItem key={relationship} value={relationship}>
-              {relationship}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
-  );
-}
+ 
 
 
-export default RelationshipContact;
+
 
 
 
